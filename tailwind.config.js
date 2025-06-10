a7353a811c2c249a4d54870179ee1436484af5ed
mastermindsjunior <admin@mastermindsjunior.com>
2025-06-10 18:10:58 +0530

    tailwind config added

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './public/**/*.html',
+  ],
+  theme: {
+    extend: {
+      screens: {
+        'bp-500': '500px',
+        'bp-900': '900px',
+        'xlg': '1152px',
+        '1440': '1440px',
+        '3xl': '1745px',
+        '4xl': '1920px',
+        '5xl': '2304px',
+      },
+    },
+  },
+  plugins: [],
+}
