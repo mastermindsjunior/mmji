ea8773e76aaeca8147731855547fd28e5c11de86
mastermindsjunior <admin@mastermindsjunior.com>
2025-07-15 10:13:25 +0530

    why change

--- src/app/components/home/WhyChooseUs.jsx
+++ src/app/components/home/WhyChooseUs.jsx
@@ -5,14 +5,14 @@ const WhyChooseUs = () => {
   return (
     <secction className=" w-full h-auto py-20 bg-gradient-to-tr from-black via-transparent to-primary-orange/20 flex justify-center items-center" >
 
-        <div className=' 2xl:max-w-7xl max-w-6xl p-0.5 rounded-2xl bg-gradient-to-t from-blue-600 via-gray-700 to-gray-800 relative overflow-hidden'>
+        <div className=' 2xl:max-w-7xl max-w-6xl p-0.5 rounded-2xl bg-gradient-to-b from-blue-600 via-gray-700 to-gray-800 relative overflow-hidden'>
         {/*  */}
-            <div className=' absolute h-0.5 left-1/2 -translate-x-1/2 top-0 bg-gradient-to-r from-transparent via-blue-600 to-transparent w-[80%]' />
-            <div className='absolute w-[50%] 2xl:h-20 h-14 rounded-b-[100%] opacity-30 bg-blue-600 blur-2xl z-10 top-0 left-1/2 -translate-x-1/2' />
+            <div className=' absolute h-0.5 left-1/2 -translate-x-1/2 bottom-0 bg-gradient-to-r from-transparent via-blue-600 to-transparent w-[80%]' />
+            <div className='absolute w-[50%] 2xl:h-20 h-14 rounded-t-[100%] opacity-30 bg-blue-600 blur-2xl z-10 bottom-0 left-1/2 -translate-x-1/2' />
         {/*  */}
-            <div className=' w-full bg-dark rounded-[15px] flex flex-col justify-center items-center gap-8 p-8'>
+            <div className=' w-full bg-dark rounded-[15px] flex flex-col justify-center items-center gap-8 p-8 '>
 
-           <div className=' flex justify-between items-center gap-2 2xl:px-8'>
+           <div className=' flex justify-between items-center gap-2 2xl:px-8 relative z-20 '>
                 <h2 className="font-jost text-4xl md:text-5xl font-bold mb-5 pb-2 textMask_br">
                     Why Choose Us?
                 </h2>
@@ -21,7 +21,7 @@ const WhyChooseUs = () => {
                 </p>
            </div>
 
-            <ul className=' w-full text-xs flex justify-center items-center flex-wrap gap-2 '>
+            <ul className=' w-full text-xs flex justify-center items-center flex-wrap gap-2 relative z-20 '>
                 <li className=' py-2 2xl:px-5 px-3 rounded-full bg-darker hover:bg-white border border-white/30 hover:text-dark transition-all duration-500 ease-in-out'>Personalized & strategic approach.</li>
                 <li className=' py-2 2xl:px-5 px-3 rounded-full bg-darker hover:bg-white border border-white/30 hover:text-dark transition-all duration-500 ease-in-out'>Creative solutions tailored to industry needs.</li>
                 <li className=' py-2 2xl:px-5 px-3 rounded-full bg-darker hover:bg-white border border-white/30 hover:text-dark transition-all duration-500 ease-in-out'>Transparent communication and reporting.</li>
